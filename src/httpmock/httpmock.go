165b1662f67e87d8b282adb82b434c6ef94bda83
Oscar Del Ben <oscar@oscardelben.com>
2013-11-17 15:29:41 +0000

    format code

--- src/httpmock/httpmock.go
+++ src/httpmock/httpmock.go
@@ -1,17 +1,17 @@
 package httpmock
 
 import (
-  "io"
-  "bytes"
-  "net/http"
+	"bytes"
+	"io"
+	"net/http"
 )
 
-type MockTransport struct{
-  Responder func(*http.Request) (*http.Response, error)
+type MockTransport struct {
+	Responder func(*http.Request) (*http.Response, error)
 }
 
 type Body struct {
-  io.Reader
+	io.Reader
 }
 
 func (Body) Close() error { return nil }
@@ -19,26 +19,25 @@ func (Body) Close() error { return nil }
 // Used to create Body strings that can be passed to http.Response.
 //    http.Response{Body: httpmock.NewBody("hello")
 func NewBody(body string) Body {
-  return Body{bytes.NewBufferString(body)}
+	return Body{bytes.NewBufferString(body)}
 }
 
 func (t *MockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-  if t.Responder != nil {
-    return t.Responder(req)
-  }
+	if t.Responder != nil {
+		return t.Responder(req)
+	}
 
-  panic("no responder registered")
+	panic("no responder registered")
 }
 
 // TODO: this is not threadsafe, execute in a mutex
 func Activate(f func(), responder func(*http.Request) (*http.Response,
-error)) {
-  originalClient := http.DefaultClient
-  mockClient := &http.Client{Transport: &MockTransport{Responder:
-  responder}}
-  http.DefaultClient = mockClient
+	error)) {
+	originalClient := http.DefaultClient
+	mockClient := &http.Client{Transport: &MockTransport{Responder: responder}}
+	http.DefaultClient = mockClient
 
-  f()
+	f()
 
-  http.DefaultClient = originalClient
+	http.DefaultClient = originalClient
 }
--- src/httpmock/httpmock_test.go
+++ src/httpmock/httpmock_test.go
@@ -1,38 +1,37 @@
 package httpmock
 
 import (
-  "testing"
-  "net/http"
-  "io/ioutil"
-  "bytes"
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
 )
 
 func TestGet(t *testing.T) {
-  var resp *http.Response
-  var err error
+	var resp *http.Response
+	var err error
 
-  f := func() { resp, err = http.Get("http://example.com") }
+	f := func() { resp, err = http.Get("http://example.com") }
 
-  responder := func(req *http.Request) (*http.Response, error) {
-     if req.URL.Host == "example.com" {
-       return &http.Response{Status: "200", Body:
-       NewBody("Hello, World!")}, nil
-     }
-     return &http.Response{}, nil
-   }
+	responder := func(req *http.Request) (*http.Response, error) {
+		if req.URL.Host == "example.com" {
+			return &http.Response{Status: "200", Body: NewBody("Hello, World!")}, nil
+		}
+		return &http.Response{}, nil
+	}
 
-  Activate(f, responder)
+	Activate(f, responder)
 
-  if err != nil {
-    t.Error("Expected no error")
-  }
+	if err != nil {
+		t.Error("Expected no error")
+	}
 
-  if resp.Status != "200" {
-    t.Error("expected 200, got ", resp.Status)
-  }
+	if resp.Status != "200" {
+		t.Error("expected 200, got ", resp.Status)
+	}
 
-  body, _ := ioutil.ReadAll(resp.Body)
-  if !bytes.Equal(body, []byte("Hello, World!")) {
-    t.Error("Expected 'Hello, World!', got ", string(body))
-  }
+	body, _ := ioutil.ReadAll(resp.Body)
+	if !bytes.Equal(body, []byte("Hello, World!")) {
+		t.Error("Expected 'Hello, World!', got ", string(body))
+	}
 }
